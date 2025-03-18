49f8baea32f09b4dbaa8de4efd38bfd547e2c9eb
Victor Kinyanjui <leslievictorius@gmail.com>
2025-03-18 14:00:47 +0000

    Learned how to configure a node project and set up routing with express

--- myApp.js
+++ myApp.js
@@ -1,13 +1,25 @@
 let express = require('express');
 let app = express();
 
+require('dotenv').config()
 
+app.get('/', (req, res) => {
+	absolutePath = __dirname + '/views/index.html';
+	res.sendFile(absolutePath);
+});
 
+assetPath = __dirname + '/public';
+app.use('/public', express.static(assetPath));
 
+app.get('/json', (req, res) => {
+	let message = "Hello json";
 
-
-
-
+	if (process.env.MESSAGE_STYLE === "uppercase") {
+		res.json({"message": message.toUpperCase()});
+	} else {
+		res.json({"message": message});
+	}
+});
 
 
 
